97fd1ee76edf1d9a24138407fbc545b03d2b975f
elizaibeth <seperreau@gmail.com>
2022-10-19 07:52:16 +0100

    updated the version

--- config/version.php
+++ config/version.php
@@ -8,14 +8,15 @@ return [
     |
     */
     'portal' =>
-        ['id' => '2.0.0'],
+        ['id' => '7.0.0'],
     'bot' =>
-        ['id' => '2.0.0'],
+        ['id' => '7.0.0'],
     'release' => [
-        'version' => '2.0.0',
-        'name' => 'Packaged McPackageFace',
-        'description' => 'Converted Lemur Engine into a composer package so that it can be easily installed in Laravel using composer',
-        'url' => 'https://github.com/lemurengine/lemurbot/releases/tag/2.0.0',
+        'version' => '7.0.0',
+        'name' => 'LemurEngine Migration',
+        'description' => 'A migration release to help you migrate your code from the LemurEngine application
+            (located here: https://github.com/theramenrobotdiscocode/lemurengine) to the LemurEngine LemurBot packages',
+        'url' => 'https://github.com/lemurengine/lemurbot/releases/tag/7.0.0',
     ],
 
 ];
