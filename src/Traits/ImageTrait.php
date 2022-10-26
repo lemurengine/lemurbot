7e51bb32115766265ba02aef3f64ad5394592be3
elizaibeth <seperreau@gmail.com>
2022-10-26 17:59:12 +0100

    quick check

--- src/Traits/ImageTrait.php
+++ src/Traits/ImageTrait.php
@@ -15,22 +15,17 @@ trait ImageTrait
         //how are the images for this model stored?
         $imageStorage = env('PUBLIC_STORAGE_ADAPTER', 'local');
 
-        if (Storage::disk($imageStorage)->exists($this->image)) {
-            if ($imageStorage=='s3') {
-                $imageUrl = Storage::disk($imageStorage)->temporaryUrl(
-                    $this->image,
-                    Carbon::now()->addMinutes(20)
-                );
-            } else {
-
-                $imageUrl = asset('avatars/'.$this->image);
-            }
-        } else {
-            if (strpos($this->image, 'widgets/')!==false) {
-                $imageUrl = url($this->image);
-            } else {
-                $imageUrl = self::getDefaultImageUrl(false);
-            }
+        if ($imageStorage=='s3') {
+            $imageUrl = Storage::disk($imageStorage)->temporaryUrl(
+                $this->image,
+                Carbon::now()->addMinutes(20)
+            );
+        }elseif (strpos($this->image, 'widgets/')!==false) {
+            $imageUrl = url($this->image);
+        }elseif (Storage::disk($imageStorage)->exists('avatars/'.$this->image)) {
+            $imageUrl = asset('avatars/'.$this->image);
+        }else {
+            $imageUrl = self::getDefaultImageUrl(false);
         }
 
         return $imageUrl;
