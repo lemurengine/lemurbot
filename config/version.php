cdf275753a3d7adae93be30d8db29afe57bf9984
elizaibeth <seperreau@gmail.com>
2022-10-29 07:24:13 +0100

    updates to version

--- config/version.php
+++ config/version.php
@@ -8,13 +8,20 @@ return [
     |
     */
     'portal' =>
-        ['id' => '9.0.3'],
+        ['id' => '9.0.4'],
     'bot' =>
-        ['id' => '9.0.3'],
+        ['id' => '9.0.4'],
     'release' => [
-        'version' => '9.0.3',
+        'version' => '9.0.4',
         'name' => 'Operation',
-        'description' => 'Fix paths and symlinks. Updated the readme and added some default images',
+        'description' => 'Fix paths and symlinks. This allows for the management of custom widgets.
+        If you are upgrading from >9.0.3 to this version then please;
+         1. delete the public/widgets folder
+         2. run the following commands
+            php artisan vendor:publish --tag=lemurbot-config --force
+            php artisan vendor:publish --tag=lemurbot-assets --force
+            php artisan vendor:publish --tag=lemurbot-widgets --force
+            php artisan storage:link',
         'url' => 'https://github.com/lemurengine/lemurbot/releases/tag/9.0.3',
     ],
 
