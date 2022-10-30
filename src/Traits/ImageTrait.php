2aaba9a8ad5a61b1d79697f49a9d86be328c1b33
elizaibeth <seperreau@gmail.com>
2022-10-30 05:45:48 +0000

    updates to get image method

--- src/Traits/ImageTrait.php
+++ src/Traits/ImageTrait.php
@@ -13,19 +13,20 @@ trait ImageTrait
     {
 
         //how are the images for this model stored?
-        $imageStorage = env('PUBLIC_STORAGE_ADAPTER', 'local');
+        $imageStorage = config('filesystems.default');
 
-        if ($imageStorage=='s3') {
+
+        if (strpos($this->image, 'widgets/')!==false) { //if this is a widget image
+            $imageUrl = url($this->image);
+        }elseif ($imageStorage=='s3') { //if we are getting the details from s3
             $imageUrl = Storage::disk($imageStorage)->temporaryUrl(
                 $this->image,
                 Carbon::now()->addMinutes(20)
             );
-        }elseif (strpos($this->image, 'widgets/')!==false) {
-            $imageUrl = url($this->image);
-        }elseif (Storage::disk($imageStorage)->exists('public/avatars/'.$this->image)) {
+        }elseif (Storage::disk($imageStorage)->exists('public/avatars/'.$this->image)) { //if this is a local image
             $imageUrl = asset('storage/avatars/'.$this->image);
         }else {
-            $imageUrl = self::getDefaultImageUrl(false);
+            $imageUrl = self::getDefaultImageUrl(false); //get the default image
         }
 
         return $imageUrl;
