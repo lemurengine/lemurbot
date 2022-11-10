b06ff0462b0f13027bc65396b24e671203d7a225
elizaibeth <seperreau@gmail.com>
2022-11-10 17:07:46 +0000

    update

--- resources/views/layouts/datatables_js.blade.php
+++ resources/views/layouts/datatables_js.blade.php
@@ -5,4 +5,4 @@
 <script src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.colVis.min.js"></script>
 <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
-<script src="{{ asset('vendor/lemurbot/js/buttons.server-side-extended.js') }}"></script>
+<script src="{{ asset('vendor/lemurbot/js/buttons.upload-extended.js') }}"></script>
