d3b42d54efd125c174fdca7c6fbe4bcc4d21e0b4
elizaibeth <seperreau@gmail.com>
2022-11-29 21:40:23 +0000

    plugin update

--- src/Models/BotPlugin.php
+++ src/Models/BotPlugin.php
@@ -158,6 +158,7 @@ class BotPlugin extends Model
             'plugins.is_master',
             'bot_plugins.bot_id'])
             ->leftjoin('bot_plugins', 'plugins.id', '=', 'bot_plugins.plugin_id')
+            ->where('bots.id', $botId)
             ->myEditableItems()
             ->orderBy('plugins.title')
             ->get();
