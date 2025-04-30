9640f4ddf724327f42bdeeed8d2993d280dbbbe0
elizaibeth <seperreau@gmail.com>
2025-04-30 06:49:39 +0100

    thinking before response

--- resources/widgets/popup/chat.js
+++ resources/widgets/popup/chat.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     const Widget = Object.create({
         create(BotId) {
             const wdg = document.createElement("div");
@@ -39,7 +39,7 @@
         const myWidgetInstance = Widget.create(botId);
         ele.appendChild(myWidgetInstance);
 
-        if(ele.dataset.host==''|| ele.dataset.host == 'undefined' || typeof ele.dataset.host  === "undefined"){
+        if (ele.dataset.host == '' || ele.dataset.host == 'undefined' || typeof ele.dataset.host === "undefined") {
             ele.dataset.host = '';
         }
 
@@ -51,20 +51,20 @@
         document.getElementById('chat-box-close').addEventListener('click', toggleChat);
 
         var settings = {
-            "url": ele.dataset.host+'/api/talk/meta',
+            "url": ele.dataset.host + '/api/talk/meta',
             "method": "POST",
             "timeout": 0,
             "headers": {
                 "Content-Type": "application/json"
             },
-            "data": JSON.stringify({"bot":botId}),
+            "data": JSON.stringify({"bot": botId}),
         };
 
         $.ajax(settings).done(function (response) {
-            if(ele.dataset.clientimage=='' || typeof ele.dataset.clientimage  === "undefined"){
+            if (ele.dataset.clientimage == '' || typeof ele.dataset.clientimage === "undefined") {
                 ele.dataset.clientimage = response.data.client.image || 'default_image.png';
             }
-            if(ele.dataset.botimage==='' || typeof ele.dataset.botimage === "undefined"){
+            if (ele.dataset.botimage === '' || typeof ele.dataset.botimage === "undefined") {
                 ele.dataset.botimage = response.data.bot.image || 'bot_avatar.png';
             }
         });
@@ -75,43 +75,70 @@
     const submitChat = (e) => {
         e.preventDefault();
         let msg = $("#chat-input").val();
-        if(msg.trim() == ''){
+        if (msg.trim() == '') {
             return false;
         }
 
         const ele = document.querySelector('#chat-popup');
-        if(ele.dataset.clientId==''|| ele.dataset.clientImage == 'undefined' ){
-            ele.dataset.clientId='';
+        if (ele.dataset.clientId == '' || ele.dataset.clientImage == 'undefined') {
+            ele.dataset.clientId = '';
         }
-        if(ele.dataset.html==''|| ele.dataset.html == 'undefined' ){
+        if (ele.dataset.html == '' || ele.dataset.html == 'undefined') {
             ele.dataset.html = 1;
         }
-        if(ele.dataset.host==''|| ele.dataset.host == 'undefined' || typeof ele.dataset.host  === "undefined"){
+        if (ele.dataset.host == '' || ele.dataset.host == 'undefined' || typeof ele.dataset.host === "undefined") {
             ele.dataset.host = '';
         }
-        if(ele.dataset.clientimage=='' || typeof ele.dataset.clientimage  === "undefined"){
+        if (ele.dataset.clientimage == '' || typeof ele.dataset.clientimage === "undefined") {
             ele.dataset.clientimage = 'default_image.png';
         }
 
-        $(".chat-logs").generateMessage(msg, ele.dataset.clientimage,'user');
+        $(".chat-logs").generateMessage(msg, ele.dataset.clientimage, 'user');
 
         const botImage = ele.dataset.botimage || 'bot_avatar.png';
         const typingMsgId = 'cm-msg-' + Date.now();
 
-        $(".chat-logs").append(`<div id="${typingMsgId}" class="chat-msg bot">
-            <span class="msg-avatar"><img src="${botImage}"></span>
-            <div class="cm-msg-text typing"></div>
-        </div>`);
-        $("#" + typingMsgId).hide().fadeIn(300);
+        const delayBeforeTyping = Math.random() * 1000 + 500; // 500ms to 1500ms
+
+        setTimeout(() => {
+            $(".chat-logs").append(`<div id="${typingMsgId}" class="chat-msg bot">
+        <span class="msg-avatar"><img src="${botImage}"></span>
+        <div class="cm-msg-text typing"></div>
+    </div>`);
+            $("#" + typingMsgId).hide().fadeIn(300);
+
+            // Start bot response delay (existing code)
+            const responseDelay = Math.random() * 2000 + 2000; // 2000ms to 4000ms
+            setTimeout(() => {
+                $.ajax(settings).done(function (response) {
+                    const botResponse = response.data.conversation.output;
+                    const botAvatar = response.data.bot.image;
+                    $("#" + typingMsgId + " .cm-msg-text").removeClass("typing").html(botResponse);
+                    $("#" + typingMsgId + " img").attr('src', botAvatar);
+
+                    $("#chat-popup").attr("data-clientId", response.data.client.id);
+                    ele.dataset.botimage = response.data.bot.image || 'bot_avatar.png';
+                    if (response.data.client.id) {
+                        ele.dataset.clientid = response.data.client.id;
+                    }
+                });
+            }, responseDelay);
+        }, delayBeforeTyping);
+
 
         var settings = {
-            "url": ele.dataset.host+'/api/talk/bot',
+            "url": ele.dataset.host + '/api/talk/bot',
             "method": "POST",
             "timeout": 0,
             "headers": {
                 "Content-Type": "application/json"
             },
-            "data": JSON.stringify({"client":ele.dataset.clientid,"bot":ele.dataset.botid,"html":ele.dataset.showHtml,"message":msg}),
+            "data": JSON.stringify({
+                "client": ele.dataset.clientid,
+                "bot": ele.dataset.botid,
+                "html": ele.dataset.showHtml,
+                "message": msg
+            }),
         };
 
         const delay = Math.random() * 1000 + 1000;
@@ -124,7 +151,7 @@
 
                 $("#chat-popup").attr("data-clientId", response.data.client.id);
                 ele.dataset.botimage = response.data.bot.image || 'bot_avatar.png';
-                if(response.data.client.id){
+                if (response.data.client.id) {
                     ele.dataset.clientid = response.data.client.id;
                 }
             });
@@ -137,27 +164,27 @@
     };
 })();
 
-$( document ).ready(function() {
+$(document).ready(function () {
     var chatMsgIndex = 0;
-    (function( $ ){
-        $.fn.generateMessage = function(msg, image, type) {
+    (function ($) {
+        $.fn.generateMessage = function (msg, image, type) {
             chatMsgIndex++;
-            let str="";
-            str += "<div id='cm-msg-"+chatMsgIndex+"' class=\"chat-msg "+type+"\">";
+            let str = "";
+            str += "<div id='cm-msg-" + chatMsgIndex + "' class=\"chat-msg " + type + "\">";
             str += "          <span class=\"msg-avatar\">";
-            str += "            <img src=\""+image+"\">";
+            str += "            <img src=\"" + image + "\">";
             str += "          <\/span>";
             str += "          <div class=\"cm-msg-text\">";
             str += msg;
             str += "          <\/div>";
             str += "        <\/div>";
             $(".chat-logs").append(str);
-            $("#cm-msg-"+chatMsgIndex).hide().fadeIn(300);
-            if(type == 'user'){
+            $("#cm-msg-" + chatMsgIndex).hide().fadeIn(300);
+            if (type == 'user') {
                 $("#chat-input").val('');
             }
-            $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
+            $(".chat-logs").stop().animate({scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
             return this;
         };
-    })( jQuery );
+    })(jQuery);
 });
