4203527852dafa6236c4a1234e1df258bc14c4f6
elizaibeth <seperreau@gmail.com>
2025-09-18 14:37:17 +0100

    updates to bot keys

--- resources/views/layouts/by_bot_edit_modal.blade.php
+++ resources/views/layouts/by_bot_edit_modal.blade.php
@@ -9,7 +9,7 @@
                     <div class="clearfix"></div>
                 </div>
                 <div class="modal-body">
-                    @include('lemurbot::'.$resourceFolder.'.fields')
+                    @include($resourceFolder.'.fields')
                     {{ Form::hidden('redirect_url', url()->current(),['data-test'=>"{$htmlTag}-edit-redirect-url"]) }}
                     <div class="clearfix"></div>
                 </div>
--- resources/views/layouts/by_bot_simple_add_modal.blade.php
+++ resources/views/layouts/by_bot_simple_add_modal.blade.php
@@ -8,7 +8,7 @@
                     <div class="clearfix"></div>
                 </div>
                 <div class="modal-body">
-                    @include('lemurbot::'.$resourceFolder.'.fields')
+                    @include($resourceFolder.'.fields')
                     {{ Form::hidden('redirect_url', url()->current(),['data-test'=>"{$htmlTag}-create-redirect-url"]) }}
                     <div class="clearfix"></div>
                 </div>
